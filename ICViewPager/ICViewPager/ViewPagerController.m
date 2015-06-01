e4a30901693c0ed631e6df0a81a09142ee2fc1e3
John <2say@macarong.net>
2015-06-01 14:08:00 +0900

    topLayoutGuide = 76.0

--- ICViewPager/ICViewPager/ViewPagerController.m
+++ ICViewPager/ICViewPager/ViewPagerController.m
@@ -204,7 +204,7 @@
     
     CGFloat topLayoutGuide = 0.0;
     if (IOS_VERSION_7) {
-        topLayoutGuide = 20.0;
+        topLayoutGuide = 76.0;
         if (self.navigationController && !self.navigationController.navigationBarHidden) {
             topLayoutGuide += self.navigationController.navigationBar.frame.size.height;
         }
